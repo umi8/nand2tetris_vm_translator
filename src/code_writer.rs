f43217fd9acbea4a26ac4941318f149cf9af07a6
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-25 10:31:24 +0900

    feat: add output file to code writer

--- src/code_writer.rs
+++ src/code_writer.rs
@@ -1,10 +1,24 @@
+use std::fs::File;
+use std::io::BufReader;
+
 pub struct CodeWriter {
+    file: File,
     stack_pointer: i32,
 }
 
 impl CodeWriter {
-    pub fn new(&mut self) {
-        self.stack_pointer = 0;
+    pub fn new(file_path: &str) -> Result<Self, &'static str> {
+        let out = File::open(file_path);
+
+        match out {
+            Ok(file) => {
+                Ok(CodeWriter {
+                    file,
+                    stack_pointer: 256,
+                })
+            }
+            Err(_) => Err("file error"),
+        }
     }
 
     pub fn write_arithmetic(command: &str) {}
