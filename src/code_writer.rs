1064de3f71298b5c01f311846db774d0de53c60b
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-25 09:45:47 +0900

    feat: impl add command

---
+++ src/code_writer.rs
@@ -0,0 +1,32 @@
+pub struct CodeWriter {
+    stack_pointer: i32,
+}
+
+impl CodeWriter {
+    pub fn new(&mut self) {
+        self.stack_pointer = 0;
+    }
+
+    pub fn write_arithmetic(command: &str) {}
+
+    pub fn write_push_pop(command: &str, segment: &str, index: i32) {}
+
+    fn add(&mut self) -> String {
+        let first = self.stack_pointer - 2;
+        let second = self.stack_pointer - 1;
+        self.stack_pointer -= 1;
+        format!("@{}\nD=M\n@{}\nM=M+D", second, first)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::code_writer::CodeWriter;
+
+    #[test]
+    fn can_add() {
+        let mut code_writer = CodeWriter { stack_pointer: 258 };
+        assert_eq!(code_writer.add(), "@257\nD=M\n@256\nM=M+D");
+        assert_eq!(code_writer.stack_pointer, 257)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod command_type;
+mod code_writer;
+
 fn main() {
     println!("Hello, world!");
 }
