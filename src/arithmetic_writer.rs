55d7c591d3fcba6e3c5729379252ad7b37334ce0
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-25 10:30:40 +0900

    feat: add arithmetic writer

---
+++ src/arithmetic_writer.rs
@@ -0,0 +1,36 @@
+fn add(stack_pointer: i32) -> String {
+    let first = stack_pointer - 2;
+    let second = stack_pointer - 1;
+    format!("@{}\nD=M\n@{}\nM=M+D", second, first)
+}
+
+fn sub(stack_pointer: i32) -> String {
+    let first = stack_pointer - 2;
+    let second = stack_pointer - 1;
+    format!("@{}\nD=M\n@{}\nM=M-D", second, first)
+}
+
+fn neg(stack_pointer: i32) -> String {
+    let target = stack_pointer - 1;
+    format!("@{}\nM=!M", target)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::arithmetic_writer::{add, neg, sub};
+
+    #[test]
+    fn can_add() {
+        assert_eq!(add(258), "@257\nD=M\n@256\nM=M+D");
+    }
+
+    #[test]
+    fn can_sub() {
+        assert_eq!(sub(258), "@257\nD=M\n@256\nM=M-D");
+    }
+
+    #[test]
+    fn can_neg() {
+        assert_eq!(neg(257), "@256\nM=!M");
+    }
+}
--- src/code_writer.rs
+++ src/code_writer.rs
@@ -11,48 +11,4 @@ impl CodeWriter {
 
     pub fn write_push_pop(command: &str, segment: &str, index: i32) {}
 
-    fn add(&mut self) -> String {
-        let first = self.stack_pointer - 2;
-        let second = self.stack_pointer - 1;
-        self.stack_pointer -= 1;
-        format!("@{}\nD=M\n@{}\nM=M+D", second, first)
-    }
-
-    fn sub(&mut self) -> String {
-        let first = self.stack_pointer - 2;
-        let second = self.stack_pointer - 1;
-        self.stack_pointer -= 1;
-        format!("@{}\nD=M\n@{}\nM=M-D", second, first)
-    }
-
-    fn neg(&mut self) -> String {
-        let target = self.stack_pointer - 1;
-        format!("@{}\nM=!M", target)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::code_writer::CodeWriter;
-
-    #[test]
-    fn can_add() {
-        let mut code_writer = CodeWriter { stack_pointer: 258 };
-        assert_eq!(code_writer.add(), "@257\nD=M\n@256\nM=M+D");
-        assert_eq!(code_writer.stack_pointer, 257)
-    }
-
-    #[test]
-    fn can_sub() {
-        let mut code_writer = CodeWriter { stack_pointer: 258 };
-        assert_eq!(code_writer.sub(), "@257\nD=M\n@256\nM=M-D");
-        assert_eq!(code_writer.stack_pointer, 257)
-    }
-
-    #[test]
-    fn can_neg() {
-        let mut code_writer = CodeWriter { stack_pointer: 257 };
-        assert_eq!(code_writer.neg(), "@256\nM=!M");
-        assert_eq!(code_writer.stack_pointer, 257)
-    }
 }
