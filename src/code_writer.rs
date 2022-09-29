9ece6c19df252cb3ce42db3cc8b20b2b4b06ba87
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-29 10:34:59 +0900

    refactor: re-impl add and push constant

--- src/code_writer.rs
+++ src/code_writer.rs
@@ -20,19 +20,41 @@ impl CodeWriter {
     }
 
     pub fn write_arithmetic(&mut self, command: &str) -> std::io::Result<()> {
-        writeln!(&mut self.file, "{}", arithmetic_writer::add())?;
-        self.write_stack_pointer()?;
+        // decrement sp
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "M=M-1")?;
+        // set sp address in A register
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "A=M")?;
+        // store top of stack value in D register
+        writeln!(&mut self.file, "D=M")?;
+
+        // decrement sp
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "M=M-1")?;
+        // set sp address in A register
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "A=M")?;
+        // store the result of add calc in A register
+        writeln!(&mut self.file, "M=M+D")?;
+        // increment sp
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "M=M+1")?;
         Ok(())
     }
 
     pub fn write_push_pop(&mut self, command: CommandType, segment: &str, index: &i32) -> std::io::Result<()> {
-        writeln!(&mut self.file, "@{}\nD=A\n@SP\nM=D", index)?;
-        self.write_stack_pointer()?;
-        Ok(())
-    }
-
-    fn write_stack_pointer(&mut self) -> std::io::Result<()> {
-        writeln!(&mut self.file, "@SP\nD=A\n@0\nM=D")?;
+        // store index in D register
+        writeln!(&mut self.file, "@{}", index)?;
+        writeln!(&mut self.file, "D=A")?;
+        // set sp address in A register
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "A=M")?;
+        // store index in stack[sp]
+        writeln!(&mut self.file, "M=D")?;
+        // increment sp
+        writeln!(&mut self.file, "@SP")?;
+        writeln!(&mut self.file, "M=M+1")?;
         Ok(())
     }
 }
