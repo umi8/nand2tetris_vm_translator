c877cc91bf7f63338c6bf3398f1749de9b1e3775
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-10-10 10:57:53 +0900

    refactor: remove common process

--- src/push_pop_writer.rs
+++ src/push_pop_writer.rs
@@ -1,6 +1,7 @@
 use std::fmt::{Error, Write};
 
 use crate::CommandType;
+use crate::segment::Segment;
 
 pub fn write(command: CommandType, segment: &str, index: &i32) -> Result<String, Error> {
     let mut s = String::new();
@@ -13,86 +14,119 @@ pub fn write(command: CommandType, segment: &str, index: &i32) -> Result<String,
 }
 
 fn push(s: &mut String, segment: &str, index: &i32) -> Result<(), Error> {
-    if segment.eq("constant") {
-        writeln!(s, "@{}", index)?;
-        writeln!(s, "D=A")?;
-        set_memory_address_to_stack_pointer(s)?;
-        writeln!(s, "M=D")?;
-        increment_stack_pointer(s)?;
-    } else if segment.eq("pointer") {
-        if *index == 0 {
+    match Segment::from(segment).unwrap() {
+        Segment::CONSTANT => {
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=A")?;
+        },
+        Segment::LOCAL => {
+            writeln!(s, "@LCL")?;
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+            writeln!(s, "A=D")?;
+            writeln!(s, "D=M")?;
+        },
+        Segment::ARGUMENT => {
+            writeln!(s, "@ARG")?;
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+            writeln!(s, "A=D")?;
+            writeln!(s, "D=M")?;
+        },
+        Segment::THIS => {
             writeln!(s, "@THIS")?;
-        } else if *index == 1 {
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+            writeln!(s, "A=D")?;
+            writeln!(s, "D=M")?;
+        },
+        Segment::THAT => {
             writeln!(s, "@THAT")?;
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+            writeln!(s, "A=D")?;
+            writeln!(s, "D=M")?;
+        },
+        Segment::TEMP => {
+            writeln!(s, "@5")?;
+            writeln!(s, "D=A")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+            writeln!(s, "A=D")?;
+            writeln!(s, "D=M")?;
+        },
+        Segment::POINTER => {
+            if *index == 0 {
+                writeln!(s, "@THIS")?;
+            } else if *index == 1 {
+                writeln!(s, "@THAT")?;
+            }
+            writeln!(s, "D=M")?;
         }
-        writeln!(s, "D=M")?;
-        set_memory_address_to_stack_pointer(s)?;
-        writeln!(s, "M=D")?;
-        increment_stack_pointer(s)?;
-    } else {
-        store_address_into_d_register(s, segment, index)?;
-        writeln!(s, "A=D")?;
-        writeln!(s, "D=M")?;
-        set_memory_address_to_stack_pointer(s)?;
-        writeln!(s, "M=D")?;
-        increment_stack_pointer(s)?;
     }
+
+    set_memory_address_to_stack_pointer(s)?;
+    writeln!(s, "M=D")?;
+    increment_stack_pointer(s)?;
     Ok(())
 }
 
 fn pop(s: &mut String, segment: &str, index: &i32) -> Result<(), Error> {
-    if segment.eq("pointer") {
-        if *index == 0 {
+    match Segment::from(segment).unwrap() {
+        Segment::CONSTANT => {} // do nothing
+        Segment::LOCAL => {
+            writeln!(s, "@LCL")?;
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+        },
+        Segment::ARGUMENT => {
+            writeln!(s, "@ARG")?;
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+        },
+        Segment::THIS => {
             writeln!(s, "@THIS")?;
-        } else if *index == 1 {
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+        },
+        Segment::THAT => {
             writeln!(s, "@THAT")?;
+            writeln!(s, "D=M")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+        },
+        Segment::TEMP => {
+            writeln!(s, "@5")?;
+            writeln!(s, "D=A")?;
+            writeln!(s, "@{}", index)?;
+            writeln!(s, "D=D+A")?;
+        },
+        Segment::POINTER => {
+            if *index == 0 {
+                writeln!(s, "@THIS")?;
+            } else if *index == 1 {
+                writeln!(s, "@THAT")?;
+            }
+            writeln!(s, "D=A")?;
         }
-        writeln!(s, "D=A")?;
-        writeln!(s, "@R13")?;
-        writeln!(s, "M=D")?;
-
-        peek_value_into_d_register(s)?;
-
-        writeln!(s, "@R13")?;
-        writeln!(s, "A=M")?;
-
-        writeln!(s, "M=D")?;
-    } else {
-        store_address_into_d_register(s, segment, index)?;
-
-        writeln!(s, "@R13")?;
-        writeln!(s, "M=D")?;
-
-        peek_value_into_d_register(s)?;
-
-        writeln!(s, "@R13")?;
-        writeln!(s, "A=M")?;
-
-        writeln!(s, "M=D")?;
-    }
-    Ok(())
-}
-
-fn store_address_into_d_register(s: &mut String, segment: &str, index: &i32) -> Result<(), Error> {
-    if segment.eq("local") {
-        writeln!(s, "@LCL")?;
-        writeln!(s, "D=M")?;
-    } else if segment.eq("argument") {
-        writeln!(s, "@ARG")?;
-        writeln!(s, "D=M")?;
-    } else if segment.eq("this") {
-        writeln!(s, "@THIS")?;
-        writeln!(s, "D=M")?;
-    } else if segment.eq("that") {
-        writeln!(s, "@THAT")?;
-        writeln!(s, "D=M")?;
-    } else if segment.eq("temp") {
-        writeln!(s, "@5")?;
-        writeln!(s, "D=A")?;
     }
 
-    writeln!(s, "@{}", index)?;
-    writeln!(s, "D=D+A")?;
+    writeln!(s, "@R13")?;
+    writeln!(s, "M=D")?;
+
+    peek_value_into_d_register(s)?;
+
+    writeln!(s, "@R13")?;
+    writeln!(s, "A=M")?;
+
+    writeln!(s, "M=D")?;
     Ok(())
 }
 
