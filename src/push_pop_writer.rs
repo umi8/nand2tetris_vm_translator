1ca0600c484c0f25baf3f3aed9bd7e72f18dec88
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-10-11 10:31:10 +0900

    refactor: common processing in push and pop

--- src/push_pop_writer.rs
+++ src/push_pop_writer.rs
@@ -14,6 +14,34 @@ pub fn write(command: CommandType, segment: Segment, index: &i32) -> Result<Stri
 }
 
 fn push(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
+    // segment[index]をDレジスタに入れる
+    store_index_of_segment_in_d_register(s, segment, index)?;
+    // SPをAレジスタに入れる
+    set_memory_address_to_stack_pointer(s)?;
+    // pushする
+    writeln!(s, "M=D")?;
+    // SPをインクリメントする
+    increment_stack_pointer(s)?;
+    Ok(())
+}
+
+fn pop(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
+    // pop先のアドレスをDレジスタに入れる
+    store_dest_address_in_d_register(s, segment, index)?;
+    // pop先のアドレスをR13に退避する
+    writeln!(s, "@R13")?;
+    writeln!(s, "M=D")?;
+    // popする値をDレジスタに入れる
+    peek_value_into_d_register(s)?;
+    // pop先のアドレスをR13から取り出してAレジスタに入れる
+    writeln!(s, "@R13")?;
+    writeln!(s, "A=M")?;
+    // popする
+    writeln!(s, "M=D")?;
+    Ok(())
+}
+
+fn store_index_of_segment_in_d_register(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
     match segment {
         Segment::CONSTANT => {
             writeln!(s, "@{}", index)?;
@@ -76,14 +104,10 @@ fn push(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
             writeln!(s, "D=M")?;
         }
     }
-
-    set_memory_address_to_stack_pointer(s)?;
-    writeln!(s, "M=D")?;
-    increment_stack_pointer(s)?;
     Ok(())
 }
 
-fn pop(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
+fn store_dest_address_in_d_register(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
     match segment {
         Segment::CONSTANT => {} // do nothing
         Segment::LOCAL => {
@@ -131,16 +155,6 @@ fn pop(s: &mut String, segment: Segment, index: &i32) -> Result<(), Error> {
             writeln!(s, "D=D+A")?;
         }
     }
-
-    writeln!(s, "@R13")?;
-    writeln!(s, "M=D")?;
-
-    peek_value_into_d_register(s)?;
-
-    writeln!(s, "@R13")?;
-    writeln!(s, "A=M")?;
-
-    writeln!(s, "M=D")?;
     Ok(())
 }
 
