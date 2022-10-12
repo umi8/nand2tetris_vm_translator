84a02d1e6cc0ba41a54afa86b891de1ba7d43178
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-10-12 09:55:28 +0900

    refactor: code_writer error handling

--- src/code_writer.rs
+++ src/code_writer.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Error, Write};
 
 use crate::{arithmetic_writer, push_pop_writer};
 use crate::arithmetic_type::ArithmeticType;
@@ -13,18 +13,12 @@ pub struct CodeWriter {
 }
 
 impl CodeWriter {
-    pub fn new(file_path: &str) -> Result<Self, MyError> {
-        let out = File::create(file_path);
-
-        match out {
-            Ok(file) => {
-                Ok(CodeWriter {
-                    file,
-                    comparison_counter: 0,
-                })
-            }
-            Err(e) => Err(MyError::Io(e))
-        }
+    pub fn new(file_path: &str) -> Result<Self, Error> {
+        let out = File::create(file_path)?;
+        Ok(CodeWriter {
+            file: out,
+            comparison_counter: 0,
+        })
     }
 
     pub fn write_arithmetic(&mut self, arithmetic_command: ArithmeticType) -> Result<(), MyError> {
