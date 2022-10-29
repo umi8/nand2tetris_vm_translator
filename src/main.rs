d1ca9c1cbe8acea3524da990f5a6275918341f5f
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-10-29 21:49:01 +0900

    feat: translate file and directory

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use walkdir::{DirEntry, WalkDir};
 use crate::arithmetic_type::ArithmeticType;
 use crate::code_writer::CodeWriter;
 use crate::command_type::CommandType;
-use crate::my_error::{IllegalArgumentError, MyError};
+use crate::my_error::MyError;
 use crate::parser::Parser;
 use crate::segment::Segment;
 
@@ -29,13 +29,15 @@ fn main() -> Result<(), MyError> {
         return Ok(());
     }
 
-    let mut code_writer = CodeWriter::new("File.asm")?;
+    let output_file_name = create_output_file_name(path);
+
+    let mut code_writer = CodeWriter::new(&output_file_name)?;
     code_writer.write_init()?;
     for file in files {
         parse(&mut code_writer, file.path().to_str().unwrap())?;
     }
 
-    println!("File translation succeeded: File.asm");
+    println!("File translation succeeded: {}", output_file_name);
     Ok(())
 }
 
@@ -55,18 +57,14 @@ fn is_vm_file(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn create_output_file_name(path: &Path) -> Result<String, IllegalArgumentError> {
+fn create_output_file_name(path: &Path) -> String {
     if path.is_file() && path.extension().unwrap() == "vm" {
-        let file_name = path.file_stem().unwrap().to_string_lossy();
-        return Ok(format!("{}.asm", file_name));
+        return String::from(path.with_extension("asm").to_string_lossy());
     }
 
-    if path.is_dir() {
-        let dir_name = path.file_name().unwrap().to_string_lossy();
-        return Ok(format!("{}/{}.asm", dir_name, dir_name));
-    }
-
-    Err(IllegalArgumentError)
+    let dir = path.to_string_lossy();
+    let dir_name = path.file_name().unwrap().to_string_lossy();
+    format!("{}/{}.asm", dir, dir_name)
 }
 
 fn parse(code_writer: &mut CodeWriter, file_path: &str) -> Result<(), MyError> {
@@ -101,21 +99,29 @@ mod tests {
     #[test]
     fn if_path_is_vm_file() {
         assert_eq!(
-            "file_name_test.asm",
-            create_output_file_name(Path::new("tests/resources/file_name_test.vm")).unwrap()
+            "tests/resources/file_name_test.asm",
+            create_output_file_name(Path::new("tests/resources/file_name_test.vm"))
         );
+    }
+
+    #[test]
+    fn if_path_is_vm_file_in_same_level() {
+        assert_eq!("file.asm", create_output_file_name(Path::new("file.vm")));
     }
 
     #[test]
     fn if_path_is_dir() {
         assert_eq!(
-            "dir_test/dir_test.asm",
-            create_output_file_name(Path::new("tests/resources/dir_test")).unwrap()
+            "tests/resources/dir_test/dir_test.asm",
+            create_output_file_name(Path::new("tests/resources/dir_test"))
         );
     }
 
     #[test]
-    fn if_path_is_not_allowed_file() {
-        assert!(create_output_file_name(Path::new("tests/resources/file_name_test.hoge")).is_err());
+    fn if_path_is_dir_in_same_level() {
+        assert_eq!(
+            "tests/tests.asm",
+            create_output_file_name(Path::new("tests"))
+        );
     }
 }
